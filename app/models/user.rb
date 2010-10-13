c5cdc79e777f5fa6b7f6e7743d565d7f221f8f7b
Matthew Rentz <mrentz@netfox.com>
2010-10-14 09:52:38 +1030

    added model validations and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+
+  EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_uniqueness_of :email
+  validates_format_of :email, :with => EmailRegex
+  validates_uniqueness_of :email, :case_sensitive => false
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :email
+  validates_numericality_of :ballance, :message=>"Error Message"
+
 end
