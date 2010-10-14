ce3df9b5135a2bf63edec31a9c18b74c5e9ee0e9
Matthew Rentz <mrentz@netfox.com>
2010-10-14 10:37:17 +1030

    added some more model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,21 @@ describe User do
   it "should create a new instance given valid attributes" do
     User.create!(@valid_attributes)
   end
+
+  it "should require a name" do
+    no_name_user = User.new(@valid_attributes)
+    no_name_user.should be_valid
+  end
+
+
+  it "should require a first name" do
+    no_name_user = User.new(@valid_attributes.merge(:first_name => ""))
+    no_name_user.should_not be_valid
+  end
+
+  it "should require a valid email" do
+    no_name_user = User.new(@valid_attributes.merge(:email => "asdffds"))
+    no_name_user.should_not be_valid
+  end
+
 end
