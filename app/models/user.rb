e1514021d31b083e534281312e67504959ada05f
Matthew Rentz <mrentz@netfox.com>
2010-10-14 11:31:42 +1030

    added some more validations and model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
 
   EmailRegex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_presence_of :email
   validates_uniqueness_of :email
   validates_format_of :email, :with => EmailRegex
   validates_uniqueness_of :email, :case_sensitive => false
   validates_presence_of :first_name
+  validates_length_of :first_name, :within => 1..12
   validates_presence_of :last_name
-  validates_presence_of :email
+  validates_length_of :last_name, :within => 1..12
   validates_numericality_of :ballance, :message=>"Error Message"
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe User do
   before(:each) do
     @valid_attributes = {
-      :first_name => "value for first_name",
-      :last_name => "value for last_name",
+      :first_name => "first_name",
+      :last_name => "last_name",
       :email => "value@for.email.mof",
       :ballance => 1.5
     }
@@ -22,6 +22,11 @@ describe User do
 
   it "should require a first name" do
     no_name_user = User.new(@valid_attributes.merge(:first_name => ""))
+    no_name_user.should_not be_valid
+  end
+
+  it "should require a first name under 12 characters" do
+    no_name_user = User.new(@valid_attributes.merge(:first_name => "abcdefghiklmn"))
     no_name_user.should_not be_valid
   end
 
